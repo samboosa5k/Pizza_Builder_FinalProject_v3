8c314ba8b7127d114a3f2c7cd059922aefae1253
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-11-18 18:46:27 +0100

    Major pizza changes

--- Pizza_Builder_Development/resources/js/components/builder/BurgerBuilder/BurgerBuilder.jsx
+++ Pizza_Builder_Development/resources/js/components/builder/BurgerBuilder/BurgerBuilder.jsx
@@ -13,14 +13,9 @@ const INGREDIENT_PRICES = {
     cheese: 0.4,
     meat: 1.3,
     bacon: 0.4
-
 }
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
     constructor( props ) {
         super( props );
         this.state = {
@@ -30,6 +25,7 @@ class BurgerBuilder extends Component {
                 cheese: 0,
                 meat: 0
             },
+            ingredientsList: [],
             totalPrice: 4, //base price
             purchasable: false,
             purchasing: false
@@ -40,6 +36,25 @@ class BurgerBuilder extends Component {
         this.purchaseHandler = this.purchaseHandler.bind( this );
         this.purchaseCancelHandler = this.purchaseCancelHandler.bind( this );
         this.purchaseContinueHandler = this.purchaseContinueHandler.bind( this );
+    }
+
+    componentDidMount() {
+        const doFetch = async () => {
+            const response = await fetch( '/api/ingredients' );
+            const data = await response.json();
+
+            const theObject = {};
+            const theArray = [];
+
+            for ( let key in data ) {
+                theObject[data[key].category] = 0;
+                theArray.push( {
+                    label: data[key].name, type: data[key].category
+                } );
+            }
+            this.setState( { ingredients: theObject, ingredientsList: theArray } );
+        }
+        doFetch();
     }
 
     updatePurchaseState( ingredients ) {
@@ -56,14 +71,17 @@ class BurgerBuilder extends Component {
 
     addIngredientHandler( type ) {
         const oldCount = this.state.ingredients[type];
+        console.log( 'oldcount value', this.state.ingredients[type] );
         const updatedCount = oldCount + 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
+
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
+
         this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
         this.updatePurchaseState( updatedIngredients );
     }
@@ -95,12 +113,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler() {
-        alert( 'You continue!' );
+        // alert( 'You continue!' );
+        this.props.submitOrderIngredients( this.state.ingredients );
     }
 
     render() {
-        console.log( 'BurgerBuilder.jsx', 'Reached render method' );
-
         const disabledInfo = {
             ...this.state.ingredients
         };
@@ -121,6 +138,7 @@ class BurgerBuilder extends Component {
                     </Modal>
                     <Burger ingredients={this.state.ingredients} />
                     <BuildControls
+                        ingredientsList={this.state.ingredientsList}
                         ingredientAdded={this.addIngredientHandler}
                         ingredientRemoved={this.removeIngredientHandler}
                         disabled={disabledInfo}
