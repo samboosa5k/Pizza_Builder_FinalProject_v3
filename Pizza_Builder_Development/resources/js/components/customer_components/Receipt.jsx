8e4920270ad8f6c17e1210755092010387ce6251
Jayne Phillips <jaynephillips61@gmail.com>
2019-11-22 11:12:14 +0100

    jayne receipt

--- Pizza_Builder_Development/resources/js/components/customer_components/Receipt.jsx
+++ Pizza_Builder_Development/resources/js/components/customer_components/Receipt.jsx
@@ -1,14 +1,48 @@
 import React from "react"
 
-import Header from "./checkout_components/Header.jsx"
 
 function Receipt(props) {
-    const {ingredientProps} = props;
+    
 
     return (
-        <div>
-            <Header />
+        <div className="receipt">
+        <div style={{ width: "100%", textAlign: "center" }}>
+            <div>
+                <p>**********************************</p>
+                <h2>RECEIPT</h2>
+                <p>**********************************</p>
+            </div>
+            <div className="flex">
+                <p>Order #2342</p>
+                <p>11/21/2019</p>
+                <p>12:00pm</p>
+            </div>
+            <div>
+                <p> <strong>Customer: </strong> first_name last_name</p>
+                <p> <strong>Address: </strong> street_and_housenumber postcode city</p>
+                <p> <strong>Phone Number: </strong> phone_number</p>
+            </div>
+            <div className="flex"></div>
+            <div className="flex">
+                <p>1 x Ingredient 1</p>
+                <p>€1.00</p>
+            </div>
+            <div className="flex">
+                <p>TOTAL AMOUNT</p>
+                <p>€12.00</p>
+            </div>
+            <div className="flex">
+                <p>TOTAL AMOUNT</p>
+                <p>€12.00</p>
+            </div>
+            <div style={{paddingTop:'15px'}}>***********THANK YOU***********</div>
+            
+            
+            
+         
+           
         </div>
+    </div>
     )
 }
 
