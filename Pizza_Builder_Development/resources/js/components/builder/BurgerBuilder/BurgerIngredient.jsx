fb8ddca2699e5f1e83dbb722ee20b1be19c09dd9
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-11-26 03:11:43 +0100

    Graphics complete...for now :D

--- Pizza_Builder_Development/resources/js/components/builder/BurgerBuilder/BurgerIngredient.jsx
+++ Pizza_Builder_Development/resources/js/components/builder/BurgerBuilder/BurgerIngredient.jsx
@@ -28,24 +28,85 @@ class BurgerIngredient extends Component {
                     </div>;
                 break;
             case ( 'Mozzarella' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
                 ingredient =
                     <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 64 + ( 8 * ( this.props.iteration + 1 ) ) + "px" }}>
-                        <img src="/image/oval.svg" alt="" />
+                        <img src="/image/mozzarella.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Cheddar' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 64 + ( 8 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/cheddar.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Bacon' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/bacon.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Basil' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/basil.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Olives' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/olives.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Mushroom' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/mushroom.svg" alt="" />
+                    </div>;
+            case ( 'Corn' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/corn.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Feta' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/feta.svg" alt="" />
+                    </div>;
+                break;
+            case ( 'Chili' ):
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
+                ingredient =
+                    <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
+                        <img src="/image/chili.svg" alt="" />
                     </div>;
                 break;
             case ( 'Pepperoni' ):
-                //  Set initial placement values
+                console.log( 'Burgeringredient.jsx', this.props.type );
+
                 ingredient =
                     <div className="Topping" style={{ zIndex: 100 + this.props.iteration, bottom: 96 + ( 24 * ( this.props.iteration + 1 ) ) + "px" }}>
                         <img src="/image/pepperoni.svg" alt="" />
                     </div>;
                 break;
-            case ( 'bacon' ):
-                ingredient = <div className="Bacon"></div>;
-                break;
-            case ( 'salad' ):
-                ingredient = <div className="Salad"></div>;
-                break;
+
             default:
                 ingredient = null;
         }
--- Pizza_Builder_Development/resources/js/components/builder/BurgerBuilder/Controls/BuildControls.jsx
+++ Pizza_Builder_Development/resources/js/components/builder/BurgerBuilder/Controls/BuildControls.jsx
@@ -9,7 +9,7 @@ const buildControls = ( props ) => {
     return (
         <div className="menu-customer__item BuildControls">
             <MenuLogo />
-            <p>Current Price: <strong>{props.price.toFixed( 2 )}</strong></p>
+            {/* <p>Current Price: <strong>{props.price.toFixed( 2 )}</strong></p> */}
             {
                 props.ingredientsList.map( ctrl => (
                     <BuildControl
